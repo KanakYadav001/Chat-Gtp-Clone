d6cf87f9e2556c34bbe16bf422f4803fe8590b0f
Kanak Yadav <yadavkanak109@gmail.com>
2025-08-24 00:28:39 +0530

    Install socket IO

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const app = require('./src/app')
 const DB = require('./src/db/db')
+const initSocketServer = require('./src/Socket/socket.server');
+const httpServer = require('http').createServer(app)
 DB();
 
+initSocketServer(httpServer)
 
-
-app.listen(3000,() =>{
+httpServer.listen(3000,() =>{
     console.log("Server Is Running On 3000 Port");
 })
