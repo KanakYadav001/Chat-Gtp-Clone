a8b02fb5c16d255d2134fbf77109d4520578eb54
Kanak Yadav <yadavkanak109@gmail.com>
2025-08-26 12:21:20 +0530

    user chat and Ai chat store in database

--- src/Socket/socket.server.js
+++ src/Socket/socket.server.js
@@ -3,6 +3,8 @@ const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
 const aiService  = require('../services/ai.service')
+const messageModel = require('../models/message.model')
+
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
@@ -29,8 +31,23 @@ function initSocketServer(httpServer) {
   io.on("connection", (socket) => {
    socket.on("ai-message", async (messagePayLoad)=> {
       console.log(messagePayLoad)
+
+
+      await messageModel.create({
+        chat : messagePayLoad.chat,
+        user: socket.user._id,
+        content : messagePayLoad.content,
+        role : "user"
+      })
       const response = await aiService.generateResponse(messagePayLoad.content)
 
+
+       await messageModel.create({
+        chat : messagePayLoad.chat,
+        user: socket.user._id,
+        content : response,
+        role : "model"
+      })
       socket.emit('ai-response', {
         content:response,
         chat:messagePayLoad.chat
